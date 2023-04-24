8a03021703d9a5f308ef0bd4dabd89fbf917d79c
Ace <ace@Aces-MacBook-Pro.local>
2023-04-24 17:51:20 +0800

    uploadto github

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,6 +8,7 @@ let pop = false
 let test = false
 let links = ['Home', 'Services', 'Account']
 let accounts = []
+let newArray = [1, 2, 3, 4, 5]
 
 
 function forLinks(i) {
--- src/routes/SecondComp.svelte
+++ src/routes/SecondComp.svelte
@@ -2,7 +2,7 @@
 
 
 </script>
-
+<h1>This is second comp</h1>
 
 
 
